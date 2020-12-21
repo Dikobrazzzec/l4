b002447c689cd4e390b09e22b47feafba56e1fae
Dikobrazzzec <zasxcd1307@mail.ru>
2020-12-21 10:37:36 +0300

    begin 2 part

--- l4/l4.cpp
+++ l4/l4.cpp
@@ -4,40 +4,76 @@
 
 using namespace std;
 
-void firtask() {
-	BYTE basArr[100];
+void FirstPart() {
+	BYTE Data[100];
 	BYTE basKeyArr[100];
-	int QuanBytes;
+	DWORD QuanBytes;
 	cout << " Enter quantity  of bytes" << endl;
 	cin >> QuanBytes;
-	cout << endl;
 	HCRYPTPROV a;
 	CryptAcquireContext(&a, NULL, NULL, PROV_RSA_SCHANNEL, CRYPT_VERIFYCONTEXT);
-	CryptGenRandom(a, QuanBytes, basArr);
+	CryptGenRandom(a, QuanBytes, Data);
 	CryptGenRandom(a, 100, basKeyArr);
-	BYTE* notEncrypted = new BYTE[QuanBytes];
-	for (int i = 0; i < QuanBytes; i++) {
-		notEncrypted[i] = basArr[i];
+	for (int i = 0; i < 100; i++) {
+			cout << hex << int(Data[i]);
 	}
-//	for (int i = 0; i < 100; i++) {
-//			cout << hex << int(basKeyArr[i]);
-//	}
+	cout << endl;
+	cout <<"|||||||||||||||||||||||||||||||||||||||||||" <<endl;
 	HCRYPTPROV q;
 	HCRYPTKEY keyDesk;
 	HCRYPTHASH hashDesk;
+	HCRYPTHASH hashDeskEn;
+	HCRYPTHASH hashDeskDec;
 	CryptAcquireContext(&q, NULL, NULL, PROV_RSA_SCHANNEL, CRYPT_VERIFYCONTEXT);
 	CryptCreateHash(q, CALG_MD5, 0, 0, &hashDesk);
-	if (!CryptHashData(hashDesk, basKeyArr, 100, 0)) {
-		cout << '2';
-	}
+	CryptCreateHash(q, CALG_MD5, 0, 0, &hashDeskEn);
+	CryptCreateHash(q, CALG_MD5, 0, 0, &hashDeskDec);
+	//if (!CryptHashData(hashDesk, basKeyArr, 100, 0)) {
+	//	cout << '2';
+	//}
 	if (!CryptDeriveKey(q,CALG_3DES, hashDesk, CRYPT_EXPORTABLE, &keyDesk)) {
 		cout << '1';
 	}
 	DWORD size = QuanBytes;
-	CryptEncrypt(keyDesk, hashDesk, TRUE, NULL, notEncrypted, &size, );
+	cout << size << endl;
+	if (!CryptEncrypt(keyDesk, hashDeskEn, TRUE, NULL, Data, &size, 100)) {
+		cout << '3' << endl;
+	}
+	for (int i = 0; i < 100; i++) {
+		cout << hex << int(Data[i]);
+	}
+	cout << endl;
+	cout << size << endl;
+	cout << "|||||||||||||||||||||||||||||||||||||||||||" << endl;
+	if (!CryptDecrypt(keyDesk, hashDeskDec, TRUE, 0, Data, &size)) {
+		cout << '4' << endl;
+	}
+	for (int i = 0; i < 100; i++) {
+		cout << hex << int(Data[i]);
+	}
+	cout << endl;
+	cout << size << endl;
+	cout << "|||||||||||||||||||||||||||||||||||||||||||" << endl;
+	cout << "|||||||||||||||||||||||||||||||||||||||||||" << endl;
+	for (int i = 0; i < size; i++) {
+		cout << hex << int(Data[i]);
+	}
+	cout << endl;
+}
+
+void SecondPart() {
+	HCRYPTPROV cryptDesk;
+	HCRYPTPROV q;
+	HCRYPTKEY keyDesk;
+	LPCTSTR contName = L"Containerrrr";
+	CryptAcquireContext(&cryptDesk, contName, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_NEWKEYSET);
+	if (!CryptGenKey(cryptDesk, AT_KEYEXCHANGE, CRYPT_EXPORTABLE, &keyDesk)) {
+		cout << '1' << endl;
+	}
 }
 
 int main()
 {
-	firtask();
+	FirstPart();
+	SecondPart();
 }

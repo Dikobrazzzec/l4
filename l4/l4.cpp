575648cc950ee9b49e10b066e2c7528b5b04ef4c
Dikobrazzzec <zasxcd1307@mail.ru>
2020-12-23 20:34:52 +0300

    working conteiner and encrypt

--- l4/l4.cpp
+++ l4/l4.cpp
@@ -2,115 +2,65 @@
 #include <wincrypt.h>
 #include <iostream>
 
-using namespace std;
+using namespace std; 
 
-void FirstPart() {
-	BYTE Data[100];
-	BYTE basKeyArr[100];
-	DWORD QuanBytes;
-	cout << " Enter quantity  of bytes" << endl;
-	cin >> QuanBytes;
-	HCRYPTPROV a;
-	CryptAcquireContext(&a, NULL, NULL, PROV_RSA_SCHANNEL, CRYPT_VERIFYCONTEXT);
-	CryptGenRandom(a, QuanBytes, Data);
-	CryptGenRandom(a, 100, basKeyArr);
-	for (int i = 0; i < 100; i++) {
-			cout << hex << int(Data[i]);
+
+void ContainerManipulation(BYTE data[], DWORD datasize) {
+	HCRYPTPROV contDesk;
+	HCRYPTPROV freeDesk;
+	HCRYPTKEY seckeyDesk;
+	HCRYPTKEY openkeyDesk;
+	LPCWSTR contName = L"Container";
+	if (!CryptAcquireContext(&freeDesk, 0, 0, PROV_RSA_SCHANNEL, CRYPT_VERIFYCONTEXT)) {
+		cout << "CM5      " << GetLastError();
 	}
-	cout << endl;
-	cout <<"|||||||||||||||||||||||||||||||||||||||||||" <<endl;
-	HCRYPTPROV q;
-	HCRYPTKEY keyDesk;
-	HCRYPTHASH hashDesk;
-	HCRYPTHASH hashDeskEn;
-	HCRYPTHASH hashDeskDec;
-	CryptAcquireContext(&q, NULL, NULL, PROV_RSA_SCHANNEL, CRYPT_VERIFYCONTEXT);
-	CryptCreateHash(q, CALG_MD5, 0, 0, &hashDesk);  //3 параметр!!!!!!!!!!!!
-	CryptCreateHash(q, CALG_MD5, 0, 0, &hashDeskEn);
-	CryptCreateHash(q, CALG_MD5, 0, 0, &hashDeskDec);
-	//if (!CryptHashData(hashDesk, basKeyArr, 100, 0)) {
-	//	cout << '2';
-	//}
-	if (!CryptDeriveKey(q,CALG_3DES, hashDesk, CRYPT_EXPORTABLE, &keyDesk)) {
-		cout << '1';
+	if (!CryptAcquireContext(&contDesk, contName, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_NEWKEYSET)) {
+		cout << "CM1      " <<GetLastError()<< endl;
 	}
-	DWORD size = QuanBytes;
-	cout << size << endl;
-	if (!CryptEncrypt(keyDesk, hashDeskEn, TRUE, NULL, Data, &size, 100)) {
-		cout << '3' << endl;
+
+	if (!CryptGenKey(contDesk, AT_KEYEXCHANGE, CRYPT_EXPORTABLE, &seckeyDesk)) {
+		cout << "CM2      " << endl;
 	}
-	for (int i = 0; i < 100; i++) {
-		cout << hex << int(Data[i]);
+	if (!CryptGetUserKey(contDesk, AT_KEYEXCHANGE, &openkeyDesk)) {
+		cout << "CM3      " << GetLastError() << endl;
 	}
-	cout << endl;
-	cout << size << endl;
-	cout << "|||||||||||||||||||||||||||||||||||||||||||" << endl;
-	if (!CryptDecrypt(keyDesk, hashDeskDec, TRUE, 0, Data, &size)) {
-		cout << '4' << endl;
+	cout << "secret key"<<seckeyDesk << endl;
+	cout << "openkey" << openkeyDesk << endl;
+	CryptEncrypt(seckeyDesk,0,TRUE,0,data,&datasize,100);
+	if (!CryptAcquireContext(&contDesk, contName, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_DELETEKEYSET)) {
+		cout << "CM4      " << GetLastError() << endl;
 	}
-	for (int i = 0; i < 100; i++) {
-		cout << hex << int(Data[i]);
-	}
-	cout << endl;
-	cout << size << endl;
-	cout << "|||||||||||||||||||||||||||||||||||||||||||" << endl;
-	cout << "|||||||||||||||||||||||||||||||||||||||||||" << endl;
-	for (int i = 0; i < size; i++) {
-		cout << hex << int(Data[i]);
+	for (int i = 0; i < datasize; i++) {
+		cout << hex<<int(data[i]);
 	}
 	cout << endl;
 }
 
-void SecondPart() {
-	HCRYPTPROV cryptDesk;
-	HCRYPTPROV q;
-	HCRYPTKEY keyDesk;
-	HCRYPTKEY openkeyDesk;
-	HCRYPTHASH hashDesk;
-	LPCTSTR contName = L"Name";
-	CryptAcquireContext(&q, NULL, NULL, PROV_RSA_SCHANNEL, CRYPT_VERIFYCONTEXT);
-	CryptCreateHash(q, CALG_MD5, 0, 0, &hashDesk);    //3 параметр!!!!!!!!!!!
+void  DataGen() {
 	BYTE DATA[100];
-	DWORD DataArrSize;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout <<"++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<< endl;
-	cout << " Enter quantity  of bytes" << endl;
-	cin >> DataArrSize;
-	CryptGenRandom(q, DataArrSize, DATA);
-	for (int i = 0; i < DataArrSize; i++) {
+	HCRYPTPROV dataDesk;
+	DWORD DATAsize;
+	if (!CryptAcquireContext(&dataDesk, 0, 0, PROV_RSA_SCHANNEL, CRYPT_VERIFYCONTEXT)) {
+		cout << "DG1" << endl;
+	}
+	cout << "Enter quantity of bytes" << endl;
+	cin >> DATAsize;
+	if (!CryptGenRandom(dataDesk, DATAsize, DATA)) {
+		cout << "DG2" << endl;
+	}
+	for (int i = 0; i < DATAsize; i++) {
 		cout <<hex<<int( DATA[i]);
 	}
 	cout << endl;
-	if (!CryptAcquireContext(&cryptDesk, contName, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_SILENT)) {
-		cout<<'2'<<endl;
-	}
-	if (!CryptGenKey(cryptDesk, AT_KEYEXCHANGE, CRYPT_EXPORTABLE, &keyDesk)) {
-		cout << '1' << endl;
-	}
-	if (!CryptEncrypt(keyDesk, NULL, TRUE, NULL, DATA, &DataArrSize, 100)) {
-		cout << '3' << endl;
-	}
-	cout << "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII" << endl;
-	for (int i = 0; i < DataArrSize; i++) {
-		cout << hex << int(DATA[i]);
-	}
-
-	if (!CryptGetUserKey(q, AT_KEYEXCHANGE, &openkeyDesk)) {
-		cout << '4' << endl;
-	}
-	BYTE DATABLOMB[200];
-	DWORD DATABLOMBSize = 200;
-	if (CryptExportKey(keyDesk, openkeyDesk, PRIVATEKEYBLOB, NULL, DATABLOMB, &DATABLOMBSize)) {
-		cout << '5' << endl;
-	}
-
+	ContainerManipulation(DATA, DATAsize);
 }
 
-int main()
-{
-	FirstPart();
-	SecondPart();
+
+
+
+
+
+
+void main() {
+	DataGen();
 }
